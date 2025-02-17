bfcb4b4914ed93e63da5cbd70e73c75a85cfe640
Rafael Brito <henrique8264@gmail.com>
2025-02-17 18:53:18 -0300

    tests(category): add InstantiateWithIsActive test to CategoryTest

--- tests/Devflix.Admin.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/Devflix.Admin.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -26,4 +26,31 @@ public class CategoryTest
         Assert.True(category.CreatedAt < dateTimeAfter);
         Assert.True(category.IsActive);
     }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Category - Aggregate")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        var validData = new
+        {
+            Name = "Category name",
+            Description = "Category description",
+            IsActive = isActive
+        };
+
+        var dateTimeBefore = DateTime.Now;
+        var category = new DomainEntity.Category(validData.Name, validData.Description, validData.IsActive);
+        var dateTimeAfter = DateTime.Now;
+
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default, category.Id);
+        Assert.NotEqual(default, category.CreatedAt);
+        Assert.True(category.CreatedAt > dateTimeBefore);
+        Assert.True(category.CreatedAt < dateTimeAfter);
+        Assert.Equal(isActive, category.IsActive);
+    }
 }
