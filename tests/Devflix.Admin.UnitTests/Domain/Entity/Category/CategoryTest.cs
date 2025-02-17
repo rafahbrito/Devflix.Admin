32bf7efecadb480163b3c7357577ab4efe070fb9
Rafael Brito <henrique8264@gmail.com>
2025-02-17 18:16:48 -0300

    test(category): add unit test for Category instantiation

---
+++ tests/Devflix.Admin.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,29 @@
+﻿using DomainEntity = Devflix.Admin.Domain.Entity;
+
+namespace Devflix.Admin.UnitTests.Domain.Entity.Category;
+public class CategoryTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregate")]
+    public void Instantiate()
+    {
+        var validData = new
+        {
+            Name = "Category name",
+            Description = "Category description",
+        };
+
+        var dateTimeBefore = DateTime.Now;
+        var category = new DomainEntity.Category(validData.Name, validData.Description);
+        var dateTimeAfter = DateTime.Now;
+
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEmpty(default(Guid), category.Id);
+        Assert.NotEmpty(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > dateTimeBefore);
+        Assert.True(category.CreatedAt < dateTimeAfter);
+        Assert.True(category.IsActive);
+    }
+}
