1df37308ce4b94e643157d7b4ec49aaf0ae969bc
Rafael Brito <henrique8264@gmail.com>
2025-02-18 17:04:45 -0300

    feat(category): add Activate method and update Validate visibility

--- src/Devflix.Admin.Domain/Entity/Category.cs
+++ src/Devflix.Admin.Domain/Entity/Category.cs
@@ -20,7 +20,13 @@ public class Category
         Validate();
     }
 
-    public void Validate()
+    public void Activate()
+    {
+        IsActive = true;
+        Validate();
+    }
+
+    private void Validate()
     {
         if (String.IsNullOrWhiteSpace(Name))
             throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
