1ae2023c3f45b59f46b1619baca93c1f7dfbba65
Rafael Brito <henrique8264@gmail.com>
2025-02-19 17:57:59 -0300

    tests: add DomainValidatorTest validation should pass when value is not null

---
+++ tests/Devflix.Admin.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using Devflix.Admin.Domain.Validator
+
+using FluentAssertions;
+
+namespace Devflix.Admin.UnitTests.Domain.Validation;
+public class DomainValidatorTest
+{
+    private Faker Faker {  get; set; } = new Faker();
+
+    // Validar null
+    [Fact(DisplayName = nameof(ValidateShouldPassWhenValueIsNotNull))]
+    [Trait("Domain", "DomainValidation - Validators")]
+    public void ValidateShouldPassWhenValueIsNotNull()
+    {
+        var value = Faker.Commerce.Categories(1)[0];
+
+        Action action = () => DomainValidation.NotNull(value, "Value");
+
+        action.Should().NotThrow();
+    }
+    // Validar null ou vazio
+    // Validar tamanho min
+    // Validar tamanho max
+}
