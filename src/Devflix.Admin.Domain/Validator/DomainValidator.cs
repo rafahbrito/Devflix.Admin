e057fd2883268cb003ccf9ae8ec71b7b964ed4f3
Rafael Brito <henrique8264@gmail.com>
2025-02-19 18:33:02 -0300

    feat(validation): add null check and exception in DomainValidator class

--- src/Devflix.Admin.Domain/Validator/DomainValidator.cs
+++ src/Devflix.Admin.Domain/Validator/DomainValidator.cs
@@ -1,8 +1,11 @@
-﻿namespace Devflix.Admin.Domain.Validator;
+﻿using Devflix.Admin.Domain.Exceptions;
+
+namespace Devflix.Admin.Domain.Validator;
 public class DomainValidator
 {
-    public static void NotNull(object target, string fieldName)
+    public static void NotNull(object? target, string fieldName)
     {
-
+        if (target is null)
+            throw new EntityValidationException($"{fieldName} should not be null");
     }
 }
