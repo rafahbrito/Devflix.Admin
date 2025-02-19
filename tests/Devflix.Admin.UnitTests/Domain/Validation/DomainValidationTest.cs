1561dfbbbfca26f5add014f2159486dd0a9d6e7b
Rafael Brito <henrique8264@gmail.com>
2025-02-19 19:02:46 -0300

    test: add test for null or empty string validation

--- tests/Devflix.Admin.UnitTests/Domain/Validation/DomainValidationTest.cs
+++ tests/Devflix.Admin.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -45,6 +45,23 @@ public class DomainValidatorTest
 
         action.Should().NotThrow();
     }
+
+    [Theory(DisplayName = nameof(ValidateShouldThrowExceptionWhenStringIsNullOrEmpty))]
+    [Trait("Domain", "DomainValidation - Validators")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("      ")]
+
+    public void ValidateShouldThrowExceptionWhenStringIsNullOrEmpty(string target)
+    { 
+        string fieldName = Faker.Database.Column();
+
+        Action action = () => DomainValidator.NotNullOrEmpty(target, fieldName);
+
+        action.Should()
+              .Throw<EntityValidationException>()
+              .WithMessage($"{fieldName} should not be null or empty");
+    }
     // Validar tamanho min
     // Validar tamanho max
 }
