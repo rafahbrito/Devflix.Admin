7c8874ddcbb7ebc6658fc27ead12ac0ee44ce896
Rafael Brito <henrique8264@gmail.com>
2025-02-19 22:00:00 -0300

    tests: add string length validation tests

--- tests/Devflix.Admin.UnitTests/Domain/Validation/DomainValidationTest.cs
+++ tests/Devflix.Admin.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -1,13 +1,38 @@
 ﻿using Bogus;
 using Devflix.Admin.Domain.Exceptions;
 using Devflix.Admin.Domain.Validator;
-
 using FluentAssertions;
 
 namespace Devflix.Admin.UnitTests.Domain.Validation;
 public class DomainValidatorTest
 {
     private Faker Faker {  get; set; } = new Faker();
+
+    public static IEnumerable<object[]> GetValuesSmallerThanMinLength(int numberOfTests = 5)
+    {
+        Faker faker = new();
+        Random random = new();
+
+        for (int i = 0; i < numberOfTests; i++)
+        {
+            string value = faker.Commerce.ProductName();
+            int minLength = value.Length + random.Next(1, 20);
+            yield return new object[] { value, minLength };
+        }
+    }
+
+    public static IEnumerable<object[]> GetValuesGreaterThanMinLength(int numberOfTests = 5)
+    {
+        Faker faker = new();
+        Random random = new();
+
+        for (int i = 0; i < numberOfTests; i++)
+        {
+            string value = faker.Commerce.ProductName();
+            int minLength = random.Next(1, (value.Length - 1));
+            yield return new object[] { value, minLength };
+        }
+    }
 
     [Fact(DisplayName = nameof(ValidateShouldPassWhenValueIsNotNull))]
     [Trait("Domain", "DomainValidation - Validators")]
@@ -33,7 +58,7 @@ public class DomainValidatorTest
               .Throw<EntityValidationException>()
               .WithMessage($"{fieldName} should not be null");
     }
-    // Validar null ou vazio
+
     [Fact(DisplayName = nameof(ValidateShouldPassWhenStringIsNotNullOrEmpty))]
     [Trait("Domain", "DomainValidation - Validators")]
     public void ValidateShouldPassWhenStringIsNotNullOrEmpty()
@@ -62,6 +87,31 @@ public class DomainValidatorTest
               .Throw<EntityValidationException>()
               .WithMessage($"{fieldName} should not be null or empty");
     }
-    // Validar tamanho min
+
+    [Theory(DisplayName = nameof(ValidateShouldPassWhenValueIsEqualOrGreaterThanMinLength))]
+    [Trait("Domain", "DomainValidation - Validators")]
+    [MemberData(nameof(GetValuesGreaterThanMinLength), parameters: 10)]
+    public void ValidateShouldPassWhenValueIsEqualOrGreaterThanMinLength(string target, int minLength)
+    {
+        string fieldName = Faker.Database.Column();
+
+        Action action = () => DomainValidator.MinLength(target, minLength, fieldName);
+
+        action.Should().NotThrow();
+    }
+
+    [Theory(DisplayName = nameof(ValidateShouldThrowExceptionWhenValueIsShorterThanMinLength))]
+    [Trait("Domain", "DomainValidation - Validators")]
+    [MemberData(nameof(GetValuesSmallerThanMinLength), parameters: 10)]
+    public void ValidateShouldThrowExceptionWhenValueIsShorterThanMinLength(string target, int minLength)
+    {
+        string fieldName = Faker.Database.Column();
+
+        Action action = () => DomainValidator.MinLength(target, minLength, fieldName);
+
+        action.Should()
+              .Throw<EntityValidationException>()
+              .WithMessage($"{fieldName} should be at leats {minLength} characters long");
+    }
     // Validar tamanho max
 }
