dd26728bc81360c41c5c8779ccc43c129783d302
Rafael Brito <henrique8264@gmail.com>
2025-02-20 23:31:01 -0300

    tests: add CreateCategoryTest file and unit test CreateCategory to validate category creation

---
+++ tests/Devflix.Admin.UnitTests/Application/UseCases/Category/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,49 @@
+﻿using Devflix.Admin.Domain.Repository;
+using DomainEntity = Devflix.Admin.Domain.Entity;
+using FluentAssertions;
+using Moq;
+using UseCases = Devflix.Admin.Application.UseCases.Category.CreateCategory;
+
+namespace Devflix.Admin.UnitTests.Application.UseCases.Category.CreateCategory;
+public class CreateCategoryTest
+{
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("Application", "CreateCategory - UseCases")]
+    public async Task CreateCategory()
+    {
+        var repositoryMock = new Mock<ICategoryRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var useCase = new UseCases.CreateCategory(
+            repositoryMock.Object, 
+            unitOfWorkMock.Object
+        );
+
+        var input = new CreateCategoryInput(
+            "Category name",
+            "Category Description",
+            true
+        );
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(
+            repository => repository.Insert(
+                It.IsAny<DomainEntity.Category>(),
+                It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+
+        unitOfWorkMock.Verify(
+            unitOfWork => unitOfWork.Commit(It.IsAny<CancellationToken>()),
+            Times.Once
+        );
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+    }
+}
