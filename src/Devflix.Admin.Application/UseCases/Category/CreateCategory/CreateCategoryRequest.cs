23a5b095977db5f06f41332fb6593c638c17ef7d
Rafael Brito <henrique8264@gmail.com>
2025-02-22 00:27:38 -0300

    feat(category): add CreateCategoryRequest class

---
+++ src/Devflix.Admin.Application/UseCases/Category/CreateCategory/CreateCategoryRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Devflix.Admin.Application.UseCases.Category.CreateCategory;
+public class CreateCategoryRequest
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsActive { get; set; }
+
+    public CreateCategoryRequest(
+        string name,
+        string? description = null,
+        bool isActive = true
+        )
+    {
+        Name = name;
+        Description = description ?? "";
+        IsActive = isActive;
+    }
+}
