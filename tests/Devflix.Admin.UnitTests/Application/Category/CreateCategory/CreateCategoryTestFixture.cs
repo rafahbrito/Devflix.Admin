5c31156da7cebad957340c5eefc66d9da9b4c6ab
Rafael Brito <henrique8264@gmail.com>
2025-02-22 15:17:41 -0300

    tests: add CreateCategory test fixture

---
+++ tests/Devflix.Admin.UnitTests/Application/Category/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,52 @@
+﻿using Devflix.Admin.Application.Interfaces;
+using Devflix.Admin.Application.UseCases.Category.CreateCategory;
+using Devflix.Admin.Domain.Repository;
+using Devflix.Admin.UnitTests.Common;
+using Moq;
+
+namespace Devflix.Admin.UnitTests.Application.Category.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTestFixtureCollection 
+    : ICollectionFixture<CreateCategoryTestFixture> { }
+
+public class CreateCategoryTestFixture : BaseFixture
+{
+    private readonly Random _random = new();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean() => _random.Next(2) == 0;
+
+    public CreateCategoryRequest GetValidRequest() 
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();
+
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+}
