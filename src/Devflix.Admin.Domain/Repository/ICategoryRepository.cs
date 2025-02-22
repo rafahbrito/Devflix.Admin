6b90ca57cc1b10fa4d06babdf4692a182db113f6
Rafael Brito <henrique8264@gmail.com>
2025-02-21 23:23:57 -0300

    feat(repository): add ICategoryRepository interface

---
+++ src/Devflix.Admin.Domain/Repository/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿using Devflix.Admin.Domain.Entity;
+using Devflix.Admin.Domain.SeedWork;
+
+namespace Devflix.Admin.Domain.Repository;
+public interface ICategoryRepository : IGenericRepository<Category>
+{
+}
