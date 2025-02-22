aa0fabbf01d57e27a7d03a9be324740c0e229423
Rafael Brito <henrique8264@gmail.com>
2025-02-22 14:23:42 -0300

    feat(category): add CreateCategory use case interface

---
+++ src/Devflix.Admin.Application/UseCases/Category/CreateCategory/ICreateCategoryHandler.cs
@@ -0,0 +1,5 @@
+﻿namespace Devflix.Admin.Application.UseCases.Category.CreateCategory;
+public interface ICreateCategoryHandler
+{
+    public Task<CreateCategoryResponse> Handle(CreateCategoryRequest request, CancellationToken cancellationToken);
+}
