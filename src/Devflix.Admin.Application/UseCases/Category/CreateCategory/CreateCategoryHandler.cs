df7678655c59ab5fb82af0ff897f2b00ebd25cad
Rafael Brito <henrique8264@gmail.com>
2025-02-22 14:24:19 -0300

    feat(admin): add CreateCategoryHandler and update project refs

---
+++ src/Devflix.Admin.Application/UseCases/Category/CreateCategory/CreateCategoryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Devflix.Admin.Application.Interfaces;
+using Devflix.Admin.Domain.Repository;
+using DomainEntity = Devflix.Admin.Domain.Entity;
+
+
+namespace Devflix.Admin.Application.UseCases.Category.CreateCategory;
+public class CreateCategoryHandler : ICreateCategoryHandler
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CreateCategoryHandler(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+    {
+        _categoryRepository = categoryRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<CreateCategoryResponse> Handle(CreateCategoryRequest request, CancellationToken cancellationToken)
+    {
+        var category = new DomainEntity.Category(
+            request.Name,
+            request.Description,
+            request.IsActive
+        );
+
+        await _categoryRepository.Insert(category, cancellationToken);
+        await _unitOfWork.Commit(cancellationToken);
+
+        return new CreateCategoryResponse(
+            category.Id,
+            category.Name,
+            category.Description,
+            category.IsActive,
+            category.CreatedAt
+        );
+    }
+}
