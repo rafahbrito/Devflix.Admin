8a801cc2bfca386e74c91bc121fd7891f0eac27c
Rafael Brito <henrique8264@gmail.com>
2025-02-24 23:49:34 -0300

    tests: enhance CreateCategoryTest with invalid cases

--- tests/Devflix.Admin.UnitTests/Application/Category/CreateCategory/CreateCategoryTest.cs
+++ tests/Devflix.Admin.UnitTests/Application/Category/CreateCategory/CreateCategoryTest.cs
@@ -1,9 +1,9 @@
-﻿using Devflix.Admin.Domain.Repository;
-using DomainEntity = Devflix.Admin.Domain.Entity;
+﻿using Devflix.Admin.Application.UseCases.Category.CreateCategory;
+using Devflix.Admin.Domain.Exceptions;
 using FluentAssertions;
-using UseCases = Devflix.Admin.Application.UseCases.Category.CreateCategory;
 using Moq;
-using Devflix.Admin.Application.Interfaces;
+using DomainEntity = Devflix.Admin.Domain.Entity;
+using UseCases = Devflix.Admin.Application.UseCases.Category.CreateCategory;
 
 namespace Devflix.Admin.UnitTests.Application.Category.CreateCategory;
 
@@ -15,6 +15,33 @@ public class CreateCategoryTest
     public CreateCategoryTest(CreateCategoryTestFixture fixture)
     {
         _fixture = fixture;
+    }
+
+    public static IEnumerable<object[]> GetInvalidRequests()
+    {
+        var fixture = new CreateCategoryTestFixture();
+        var invalidRequestsList = new List<object[]>();
+
+        var nameNullRequest = fixture.GetValidRequest();
+        nameNullRequest.Name = null;
+        invalidRequestsList.Add([nameNullRequest, "Name should not be empty or null"]);
+
+        var shortNameRequest = fixture.GetValidRequest();
+        shortNameRequest.Name = shortNameRequest.Name.Substring(0,2);
+        invalidRequestsList.Add([shortNameRequest, "Name should be at leats 3 characters long"]);
+
+        var longName = fixture.Faker.Lorem.Letter(256);
+        var tooLongNameRequest = fixture.GetValidRequest();
+        tooLongNameRequest.Name = longName;
+        invalidRequestsList.Add([tooLongNameRequest, "Name should be less or equal 255 characters long"]);
+
+        var longDescription = fixture.Faker.Lorem.Paragraphs(1_001);
+        var longDescriptionRequest = fixture.GetValidRequest();
+        longDescriptionRequest.Description = longDescription;
+        invalidRequestsList.Add([longDescriptionRequest, "Description should be less or equal 10000 characters long"]);
+
+
+        return invalidRequestsList;
     }
 
     [Fact(DisplayName = nameof(CreateCategory))]
@@ -51,5 +78,27 @@ public class CreateCategoryTest
         output.IsActive.Should().Be(input.IsActive);
         output.Id.Should().NotBeEmpty();
         output.CreatedAt.Should().NotBeSameDateAs(default);
+    }
+
+    [Theory(DisplayName = nameof(CreateCategoryShouldThrowExceptionWhenRequestIsInvalid))]
+    [Trait("Application", "CreateCategory - UseCases")]
+    [MemberData(nameof(GetInvalidRequests))]
+    public async Task CreateCategoryShouldThrowExceptionWhenRequestIsInvalid(
+        CreateCategoryRequest input,
+        string exceptionMessage
+        )
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var useCase = new UseCases.CreateCategoryHandler(
+            repositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+
+        Func<Task> task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should()
+                  .ThrowAsync<EntityValidationException>()
+                  .WithMessage(exceptionMessage);
     }
 }
